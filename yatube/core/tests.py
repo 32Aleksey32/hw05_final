fcb04275c7e9bbe3c08d7a775f9fd3f843bf818f
Алексей Осипов <a.l.e.k.s.e.y.2011@mail.ru>
2022-05-23 10:55:24 +0300

    1

--- yatube/core/tests.py
+++ yatube/core/tests.py
@@ -4,5 +4,5 @@ from django.test import TestCase
 class ViewTestClass(TestCase):
     def test_error_page(self):
         response = self.client.get('/nonexist-page/')
-        # Проверьте, что статус ответа сервера - 404
-        # Проверьте, что используется шаблон core/404.html
+        self.assertEqual(response.status_code, 404)
+        self.assertTemplateUsed(response, 'core/404.html')
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -148,4 +148,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
 
 CSRF_FAILURE_VIEW = 'core.views.csrf_failure'
-
