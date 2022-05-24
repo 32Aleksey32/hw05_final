ad6a5deffdb06f41f9750d534552ba6b8bfb2ad6
Алексей Осипов <a.l.e.k.s.e.y.2011@mail.ru>
2022-05-24 13:53:39 +0300

    2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -107,7 +107,8 @@ def follow_index(request):
 @login_required
 def profile_follow(request, username):
     author = get_object_or_404(User, username=username)
-    if request.user != author:
+    follower = Follow.objects.filter(user=request.user, author=author)
+    if request.user != author and not follower.exists():
         Follow.objects.create(user=request.user, author=author)
     return redirect('posts:profile', username=username)
 
