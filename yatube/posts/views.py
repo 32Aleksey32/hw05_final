bd308c24dad554cc8550318117d413f3d55cd110
Алексей Осипов <a.l.e.k.s.e.y.2011@mail.ru>
2022-05-24 13:56:31 +0300

    2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -116,6 +116,7 @@ def profile_follow(request, username):
 @login_required
 def profile_unfollow(request, username):
     author = get_object_or_404(User, username=username)
+    follower = Follow.objects.filter(user=request.user, author=author)
     if request.user != author:
-        Follow.objects.filter(user=request.user, author=author).delete()
+        follower.delete()
     return redirect('posts:profile', username=username)
